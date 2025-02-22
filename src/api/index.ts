b43fe7ee1a522f2b68d33d0468f502b99f3a3328
mikhael-remboski <mikhael.remboski@justo.mx>
2025-02-22 15:31:19 -0300

    fix: add cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,11 @@ import requestContextMiddleware from '#common/middlewares/request-context/reques
 import openApiRouter from '#api/openapi/openapi.router';
 import urlShortenerRouter from '#api/routers/url-shortener.router';
 import { urlShortenController } from '#api/inject/controllers';
+import cors from 'cors';
 
 const bootstrap = (): Application => {
   const app: express.Application = express();
+  app.use(cors());
   app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true }));
   app.disable('x-powered-by');
